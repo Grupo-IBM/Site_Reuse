bb4c2161a18ec580e75d055a7d0d72a744613fc0
enzomshiotuqui <enzomshiotuqui@gmail.com>
2023-10-20 14:13:09 -0300

    feat: footer feito e reponsividade aplicada

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,10 @@
 import Cabecalho from "../components/Cabecalho/Cabecalho"
-
+import Rodape from "../components/Rodape/Rodape"
 export default function Home(){
     return(
         <>
             <Cabecalho />
-                
+            <Rodape/> 
         </>
     )
 }
